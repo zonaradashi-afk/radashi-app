2cd347c529cf2f25ee077ed68edf436eb0e76727
zonaradashi-afk <zona.radashi@gmail.com>
2026-06-15 18:44:27 -0600

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF7mM_TMjxI6QviBck5RxvT8y8cfF27nY",
+  authDomain: "zona-radashi-app.firebaseapp.com",
+  projectId: "zona-radashi-app",
+  storageBucket: "zona-radashi-app.firebasestorage.app",
+  messagingSenderId: "694078209257",
+  appId: "1:694078209257:web:dabd65fa36a02f6a0e0478",
+  measurementId: "G-BY2DK5HRYQ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
