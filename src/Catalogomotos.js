331d4f43b39c66ef1842f98fc75389cc27f50c9c
zonaradashi-afk <zona.radashi@gmail.com>
2026-06-20 12:26:34 -0600

    Create Catalogomotos.js

---
+++ src/Catalogomotos.js
@@ -0,0 +1,320 @@
+export const CATALOGO_MOTOS = {
+  "Bajaj": {
+    "Boxer 150": [
+      {
+        "version": "BM150",
+        "id": "bajaj_boxer_150_bm150"
+      }
+    ],
+    "Dominar 250": [
+      {
+        "version": "FI",
+        "id": "bajaj_dominar_250_fi"
+      }
+    ],
+    "Dominar 400": [
+      {
+        "version": "FI",
+        "id": "bajaj_dominar_400_fi"
+      }
+    ],
+    "Pulsar 125": [
+      {
+        "version": "Neon/NS",
+        "id": "bajaj_pulsar_125_neonns"
+      }
+    ],
+    "Pulsar NS125": [
+      {
+        "version": "México común",
+        "id": "bajaj_pulsar_ns125_mexico_comun"
+      }
+    ],
+    "Pulsar NS160": [
+      {
+        "version": "FI",
+        "id": "bajaj_pulsar_ns160_fi"
+      }
+    ],
+    "Pulsar NS200": [
+      {
+        "version": "FI/ABS",
+        "id": "bajaj_pulsar_ns200_fiabs"
+      }
+    ]
+  },
+  "CFMOTO": {
+    "250NK": [
+      {
+        "version": "FI",
+        "id": "cfmoto_250nk_fi"
+      }
+    ],
+    "450NK": [
+      {
+        "version": "FI",
+        "id": "cfmoto_450nk_fi"
+      }
+    ],
+    "450SR": [
+      {
+        "version": "FI",
+        "id": "cfmoto_450sr_fi"
+      }
+    ],
+    "800MT": [
+      {
+        "version": "FI",
+        "id": "cfmoto_800mt_fi"
+      }
+    ]
+  },
+  "Honda": {
+    "CB125F": [
+      {
+        "version": "México común",
+        "id": "honda_cb125f_mexico_comun"
+      }
+    ],
+    "CB190R": [
+      {
+        "version": "México común",
+        "id": "honda_cb190r_mexico_comun"
+      }
+    ],
+    "Cargo 150": [
+      {
+        "version": "GL150 Cargo",
+        "id": "honda_cargo_150_gl150_cargo"
+      }
+    ],
+    "PCX160": [
+      {
+        "version": "2024-2026",
+        "id": "honda_pcx160_20242026"
+      }
+    ],
+    "Wave 110": [
+      {
+        "version": "México común",
+        "id": "honda_wave_110_mexico_comun"
+      }
+    ],
+    "XR150L": [
+      {
+        "version": "Carburada común",
+        "id": "honda_xr150l_carburada_comun"
+      }
+    ],
+    "XR190L": [
+      {
+        "version": "México común",
+        "id": "honda_xr190l_mexico_comun"
+      }
+    ]
+  },
+  "Italika": {
+    "150Z": [
+      {
+        "version": "Carburada común",
+        "id": "italika_150z_carburada_comun"
+      }
+    ],
+    "200Z": [
+      {
+        "version": "Carburada común",
+        "id": "italika_200z_carburada_comun"
+      }
+    ],
+    "250Z": [
+      {
+        "version": "Carburada/FI según año",
+        "id": "italika_250z_carburadafi_segun_ano"
+      }
+    ],
+    "DM200": [
+      {
+        "version": "Carburada común",
+        "id": "italika_dm200_carburada_comun"
+      }
+    ],
+    "DT150": [
+      {
+        "version": "Carburada común",
+        "id": "italika_dt150_carburada_comun"
+      }
+    ],
+    "FT125": [
+      {
+        "version": "Carburada común",
+        "id": "italika_ft125_carburada_comun"
+      }
+    ],
+    "FT150": [
+      {
+        "version": "Carburada común",
+        "id": "italika_ft150_carburada_comun"
+      }
+    ],
+    "Vort-X 200": [
+      {
+        "version": "Adventure",
+        "id": "italika_vortx_200_adventure"
+      }
+    ]
+  },
+  "Suzuki": {
+    "AX100": [
+      {
+        "version": "2T",
+        "id": "suzuki_ax100_2t"
+      }
+    ],
+    "Burgman 125": [
+      {
+        "version": "Scooter",
+        "id": "suzuki_burgman_125_scooter"
+      }
+    ],
+    "GN125": [
+      {
+        "version": "Carburada común",
+        "id": "suzuki_gn125_carburada_comun"
+      }
+    ],
+    "Gixxer 150": [
+      {
+        "version": "FI",
+        "id": "suzuki_gixxer_150_fi"
+      }
+    ],
+    "Gixxer SF": [
+      {
+        "version": "FI",
+        "id": "suzuki_gixxer_sf_fi"
+      }
+    ],
+    "V-Strom 250": [
+      {
+        "version": "FI",
+        "id": "suzuki_vstrom_250_fi"
+      }
+    ]
+  },
+  "TVS": {
+    "Apache RTR 160": [
+      {
+        "version": "FI",
+        "id": "tvs_apache_rtr_160_fi"
+      }
+    ],
+    "Apache RTR 200 4V": [
+      {
+        "version": "FI",
+        "id": "tvs_apache_rtr_200_4v_fi"
+      }
+    ],
+    "Raider 125": [
+      {
+        "version": "FI",
+        "id": "tvs_raider_125_fi"
+      }
+    ]
+  },
+  "Veloci": {
+    "Razor 250": [
+      {
+        "version": "Naked",
+        "id": "veloci_razor_250_naked"
+      }
+    ],
+    "Xeverus 250": [
+      {
+        "version": "Carburada/FI",
+        "id": "veloci_xeverus_250_carburadafi"
+      }
+    ],
+    "Xeverus 300": [
+      {
+        "version": "FI común",
+        "id": "veloci_xeverus_300_fi_comun"
+      }
+    ]
+  },
+  "Vento": {
+    "Crossmax 220": [
+      {
+        "version": "Doble propósito",
+        "id": "vento_crossmax_220_doble_proposito"
+      }
+    ],
+    "Nitrox 200": [
+      {
+        "version": "Carburada común",
+        "id": "vento_nitrox_200_carburada_comun"
+      }
+    ],
+    "Rocketman 250": [
+      {
+        "version": "Carburada común",
+        "id": "vento_rocketman_250_carburada_comun"
+      }
+    ],
+    "Workman 150": [
+      {
+        "version": "Carburada común",
+        "id": "vento_workman_150_carburada_comun"
+      }
+    ]
+  },
+  "Yamaha": {
+    "FZ-S FI": [
+      {
+        "version": "2025",
+        "id": "yamaha_fzs_fi_2025"
+      }
+    ],
+    "MT15": [
+      {
+        "version": "2025-2026",
+        "id": "yamaha_mt15_20252026"
+      }
+    ],
+    "NMAX": [
+      {
+        "version": "155",
+        "id": "yamaha_nmax_155"
+      }
+    ],
+    "R15": [
+      {
+        "version": "V3/V4",
+        "id": "yamaha_r15_v3v4"
+      }
+    ],
+    "R3": [
+      {
+        "version": "2019+",
+        "id": "yamaha_r3_2019"
+      }
+    ],
+    "XTZ125": [
+      {
+        "version": "Carburada común",
+        "id": "yamaha_xtz125_carburada_comun"
+      }
+    ],
+    "XTZ150": [
+      {
+        "version": "FI según año",
+        "id": "yamaha_xtz150_fi_segun_ano"
+      }
+    ],
+    "YBR125": [
+      {
+        "version": "Carburada común",
+        "id": "yamaha_ybr125_carburada_comun"
+      }
+    ]
+  }
+};
